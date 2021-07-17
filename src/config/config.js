e856d7c949a6b6c65963f8464c87df18978f8acc
Edward Jimenez <edward.jimenez@blackboard.com>
2021-07-17 03:20:08 -0500

    Hardcoded IP

--- src/config/config.js
+++ src/config/config.js
@@ -5,19 +5,25 @@ let configuration = {};
 let configJsonOverride = null;
 
 if (process.env.CONFIG_OVERRIDE) {
-    try {
-        configJsonOverride = require(process.env.CONFIG_OVERRIDE);
-    } catch (e) {
-        // Do nothing
-    }
+  try {
+    configJsonOverride = require(process.env.CONFIG_OVERRIDE);
+  } catch (e) {
+    // Do nothing
+  }
 }
 
 if (configJsonOverride) {
-    configuration = _.defaultsDeep(configJsonOverride, configJson);
+  configuration = _.defaultsDeep(configJsonOverride, configJson);
 }
 
-if (process.env.BACKEND_URL) {
-    configuration['backend_url'] = process.env.BACKEND_URL
-}
+if (process.env.URL_SCHEMA)
+  configuration['url_schema'] = process.env.URL_SCHEMA;
+if (process.env.BACKEND_HOST)
+  configuration['backend_host'] = process.env.BACKEND_HOST;
+if (process.env.BACKEND_PORT)
+  configuration['backend_port'] = process.env.BACKEND_PORT;
+
+configuration['backend_url'] = configuration['url_schema'] + "://" + configuration['backend_host']
+  + ":" + configuration["backend_port"]
 
 export default _.defaultsDeep(configuration, configJson);
